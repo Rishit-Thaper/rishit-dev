f75132e9ab1269807bf891df60cd616fff695277
Rishit-Thaper <rishitthaper5329@gmail.com>
2024-09-05 00:14:31 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import Footer from '@/components/Footer';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/rishit.png" />
+        <title>Rishit</title>
+      </head>
       <body style={{ backgroundColor: `${colors.main}`, color: `${colors.text}`, fontFamily: `${fonts.body}` }}>
         <Navbar />
         <Providers>{children}</Providers>
